0164d91e8ef86b7f3641c48cede8360a35fea501
John-Quinones <egrpanama@gmail.com>
2022-12-11 21:40:23 -0500

    commit3

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -71,7 +71,7 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  return str1.legth === str2.legth;
 }
 
 function menosQueNoventa(num) {
