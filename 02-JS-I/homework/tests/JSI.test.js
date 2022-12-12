064aaf8909e6b11e41dfaafbc0d8e9e557f932d4
John-Quinones <egrpanama@gmail.com>
2022-12-11 23:12:05 -0500

    commit35

--- 02-JS-I/homework/tests/JSI.test.js
+++ 02-JS-I/homework/tests/JSI.test.js
@@ -80,8 +80,8 @@ describe('devolverString(str)', function() {
 
 describe('sumar(x, y)', function() {
   it('Deberia devolver la suma de los dos argumentos', function() {
-    expect(suma(5, 5)).toBe(10);
-    expect(suma(-1, 5)).toBe(4);
+    expect(sumar(5, 5)).toBe(10);
+    expect(sumar(-1, 5)).toBe(4);
   });
 });
 
