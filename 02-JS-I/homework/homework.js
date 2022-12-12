e1d6be5b1701b517fa3f70cd145f0b6ac597d625
John-Quinones <egrpanama@gmail.com>
2022-12-11 22:34:00 -0500

    commit24

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -180,7 +180,7 @@ function esPositivo(numero) {
     return "Es positivo";
   }
   else {
-    return "Es negativo",
+    return "Es negativo";
   }
 }
 
@@ -188,6 +188,7 @@ function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
+  return str + "!";
 }
 
 function combinarNombres(nombre, apellido) {
